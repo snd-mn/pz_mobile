9603729af704c9801d2b865eac5fd73b13f67220
officer-merge <anmu7c4@gmail.com>
2021-08-15 14:38:26 +0200

    PB-17: can now access the game webservice

--- Source/pz_mobile/HttpComponent.cpp
+++ Source/pz_mobile/HttpComponent.cpp
@@ -23,9 +23,6 @@ UHttpComponent::UHttpComponent()
 void UHttpComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
 }
 
 
@@ -43,65 +40,20 @@ static FString* blbbb = new FString(blub);
 /*Http call*/
 void UHttpComponent::MyHttpCall()
 {
-	// TSharedRef<IHttpRequest> Request = Http->CreateRequest();
 	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = Http->CreateRequest();
+	
+	Request->SetURL(TEXT("http://127.0.0.1:12224/tiles"));
+	Request->SetVerb(TEXT("POST"));
+	Request->SetHeader(TEXT("User-Agent"), TEXT("X-UnrealEngine-Agent"));
+	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	Request->SetHeader(TEXT("Authorization"), TEXT("Basic dXNlckBsb2NhbC10cmluaXR5Lm9yZzpzdXBlcnNhZmVwYXNzd29yZA=="));
+	
+	FString payload (TEXT("{\"gps\":{\"lat\":\"51.50889155160364\",\"lon\":\"7.464675695954779\"}}\0"));
+	
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *payload)
+	Request->SetContentAsString(*payload);
+
 	Request->OnProcessRequestComplete().BindUObject(this, &UHttpComponent::OnResponseReceived);
-	//This is the url on which to process the request
-	
-
-
-	
-	
-	// Request->SetURL(TEXT("http://127.0.0.1:12224/tiles"));
-	// Request->SetVerb(TEXT("GET"));
-	// Request->SetHeader(TEXT("User-Agent"), TEXT("X-UnrealEngine-Agent"));
-	// Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
-	// Request->SetHeader(TEXT("Authorization"), TEXT("Basic dXNlckBsb2NhbC10cmluaXR5Lm9yZzpzdXBlcnNhZmVwYXNzd29yZA=="));
-	//
-	// FString asdf = TEXT("{	\"gps\":{\"lat\":\"51.50889155160364\",	\"lon\":\"7.464675695954779\"}}");
-	// UE_LOG(LogTemp, Warning, TEXT("%s"), *asdf)
-	// Request->SetContentAsString(*blbbb);
-
-
-	// Request->SetURL(TEXT("http://127.0.0.1:12224/pingbody"));
-	// Request->SetVerb(TEXT("GET"));
-	// Request->SetHeader(TEXT("User-Agent"), TEXT("X-UnrealEngine-Agent"));
-	// Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
-	// Request->SetHeader(TEXT("Authorization"), TEXT("Basic dXNlckBsb2NhbC10cmluaXR5Lm9yZzpzdXBlcnNhZmVwYXNzd29yZA=="));
-	// Request->SetContentAsString("dummy text");
-
-	
-	// UE_LOG(LogTemp, Warning, TEXT("%s"), Request->GetContent())
-	// BytesToString(Request->GetContent());
-	// FString fromBlob = FString::FromBlob(Request->GetContent().GetData(), Request->GetContentLength())
-	// UE_LOG(LogTemp, Warning, TEXT("%s"), fromBlob)
-
-	// char ansiiData[1024]; //A temp buffer for the data
- //
-	// memcpy(ansiiData, Request->GetContent().GetData(), Request->GetContentLength()); //Assumes bytesRead is always smaller than 1024 bytes
- //
-	// ansiiData[Request->GetContentLength()] = 0; //Add null terminator
- //
-	// FString debugData = ANSI_TO_TCHAR(ansiiData); //Convert to FString
-	// UE_LOG(LogTemp, Warning, TEXT("%s"), debugData)
-
-	
-	// FString ret;
-	// for (uint8 Content : Request->GetContent())
-	// {
-	// 	char c = Content;
-	// 	std::string str = c ;
-	// 	FString append = FString();
-	// 	ret.Append(append);
-	// }
-	
-	// UE_LOG(LogTemp, Warning, TEXT("%s"), ret)
-	
-	// Request->SetURL(TEXT("http://127.0.0.1:12224/ping"));
-	// Request->SetVerb(TEXT("GET"));
-	// Request->SetHeader(TEXT("User-Agent"), TEXT("X-UnrealEngine-Agent"));
-	// Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
-	// Request->SetHeader(TEXT("Authorization"), TEXT("Basic dXNlckBsb2NhbC10cmluaXR5Lm9yZzpzdXBlcnNhZmVwYXNzd29yZA=="));
 
 	Request->ProcessRequest();
 }
@@ -109,20 +61,6 @@ void UHttpComponent::MyHttpCall()
 /*Assigned function on successfull http call*/
 void UHttpComponent::OnResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
 {
-	// //Create a pointer to hold the json serialized data
-	// TSharedPtr<FJsonObject> JsonObject;
-	//
-	// //Create a reader pointer to read the json data
-	// TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
-	//
-	// //Deserialize the json data given Reader and the actual object to deserialize
-	// if (FJsonSerializer::Deserialize(Reader, JsonObject))
-	// {
-	// 	//Get the value of the json object by field name
-	// 	
-	// 	//Output it to the engine
 	UE_LOG(LogTemp, Warning, TEXT("%s"), *Response->GetContentAsString())
-
 	GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, Response->GetContentAsString());
-	// }
 }
